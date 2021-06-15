eeb8bc77bdc608efb0964968ec65578be1164713
AlloyDome <77043817+AlloyDome@users.noreply.github.com>
2021-06-15 20:36:17 +0800

    Add files via upload

---
+++ lang/zh/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * 简体中文 · zh
+ * 
+ * @license		MIT ()
+ *
+ * @author		AlloyDome
+ */
+$lang['menu'] = '附加 CSS 样式编辑器';
+
+$lang['textareaHintLabel'] = '在这里输入附加的 CSS 样式';
+$lang['updateButton'] = '✅ 更新';
