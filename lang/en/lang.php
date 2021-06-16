b33f46db2a0cfaa5b1fa58d2c56a99528040c1ff
AlloyDome <77043817+AlloyDome@users.noreply.github.com>
2021-06-16 14:34:05 +0800

    Add files via upload

---
+++ lang/en/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * 英语 · en
+ * 
+ * @license		MIT License
+ *
+ * @author		AlloyDome
+ */
+$lang['menu'] = 'Additional CSS Style Editor';
+
+$lang['textareaHintLabel'] = 'Enter additional CSS styles here';
+$lang['updateButton'] = '✅ Update';
